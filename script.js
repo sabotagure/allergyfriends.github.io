78e2a359bc5fece7576190b97b9acf2ca56a03af
sabotagure <157576891+sabotagure@users.noreply.github.com>
2024-01-26 07:35:43 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+function toggleSection(sectionId) {
+    // Hide all sections
+    document.querySelectorAll('section').forEach(section => {
+        section.classList.add('hidden');
+    });
+
+    // Show the selected section
+    document.getElementById(sectionId).classList.remove('hidden');
+}
 
