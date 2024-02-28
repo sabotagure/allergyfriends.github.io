d4a9bce6145abe5ef24ee3aafab7ef1dc1fd92d1
sabotagure <157576891+sabotagure@users.noreply.github.com>
2024-02-28 14:59:03 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const menuItems = {
     burgers: ['Friends             MAITO  KANANMUNA  GLUTEENI  SELLERI  SINAPPI', 'Cheese Onion             MAITO  KANANMUNA  GLUTEENI  SELLERI  SINAPPI', 'Classic             MAITO  KANANMUNA  GLUTEENI  SELLERI  SINAPPI', 'Aioli             MAITO  KANANMUNA  GLUTEENI  SINAPPI', 'Bacon             MAITO  KANANMUNA  GLUTEENI  SINAPPI', 'Umami             MAITO  KANANMUNA  GLUTEENI  SINAPPI  SOIJA', 'Chipotle             MAITO  KANANMUNA  GLUTEENI  SINAPPI', 'Kids             MAITO  KANANMUNA  GLUTEENI  SELLERI', 'Chicken Aioli             KANANMUNA  GLUTEENI  SINAPPI', 'Halloumi             MAITO  KANANMUNA  GLUTEENI  SINAPPI', 'Vegan             GLUTEENI  SINAPPI'],
     sweets: ['Shakes             MAITO  LAKTOOSI  KANANMUNA', 'Jäätelö             MAITO  LAKTOOSI  KANANMUNA'],
-    sides: ['Chicken Fingers             KANANMUNA', 'Side Salad             Sinappi', 'Halloumi Fries             MAITO', 'Topped Fries             MAITO  SINAPPI  KANANMUNA', 'Truffle Fries             MAITO  SINAPPI  KANANMUNA  SOIJA'],
+    sides: ['Chicken Fingers             KANANMUNA', 'Side Salad             SINAPPI', 'Halloumi Fries             MAITO', 'Topped Fries             MAITO  SINAPPI  KANANMUNA', 'Truffle Fries             MAITO  SINAPPI  KANANMUNA  SOIJA'],
     salads: ['Crispy Chicken Ranch             MAITO  KANANMUNA  SINAPPI', 'Grilled Chicken             MAITO  KANANMUNA  SINAPPI', 'Halloumi             MAITO  KANANMUNA  SINAPPI'],
 };
 
